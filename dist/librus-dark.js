b2fac33f873b4d282625c90aca302d910df3e552
Olivier Marszałkowski <marszalkowskiolivier@gmail.com>
2023-01-10 12:00:42 +0100

    librus style

--- dist/librus-dark.js
+++ dist/librus-dark.js
@@ -1,2 +1,3 @@
 document.head.innerHTML =
-    document.head.innerHTML + '\n<link rel="stylesheet" href="../css/main.css">';
+    document.head.innerHTML +
+        '\n<link rel="stylesheet" type="text/css" crossorgin="anonymous" href="https://raw.githack.com/xybl3/Librus-Utils/main/dist/css/librus-dark.css">';
