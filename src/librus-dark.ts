6e566e22171b192c495ed4663c73f26363e54576
Olivier Marszałkowski <marszalkowskiolivier@gmail.com>
2023-01-13 18:54:09 +0100

    idk just something that i don't remember

--- src/librus-dark.ts
+++ src/librus-dark.ts
@@ -1,3 +1,3 @@
 document.head.innerHTML =
   document.head.innerHTML +
-  '\n<link rel="stylesheet" type="text/css" crossorgin="anonymous" href="https://rawcdn.githack.com/xybl3/Librus-Utils/b2fac33f873b4d282625c90aca302d910df3e552/dist/css/librus-dark.css">';
+  `\n<link rel="stylesheet" type="text/css" crossorgin="anonymous" href="https://93ab-31-6-212-184.eu.ngrok.io/css/librus-dark.css"`;
